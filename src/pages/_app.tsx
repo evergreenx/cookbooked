f588a165dea1438b2658724126e91ad9d02932a8
ido evergreen <dicksonevergreenido@gmail.com>
2023-06-03 18:19:19 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
 
 <Head>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
   </Head>
 
 
