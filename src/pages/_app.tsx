b48e2b795fe24f9f9f3de66c65a5f865ca1c5280
ido evergreen <dicksonevergreenido@gmail.com>
2023-06-03 18:14:39 +0100

    Add head to page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@/providers/AuthProviders";
-
+import Head from "next/head"
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    
+    <>
+
+<Head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  </Head>
+
+
+
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
+</>
   );
 }
